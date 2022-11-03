e219cbd35338b7cf7a7aba272ae0141b075e0692
UrielRabih <105129417+UrielRabih@users.noreply.github.com>
2022-11-03 09:41:12 -0300

    registrarse

--- screens/Registrarse.jsx
+++ screens/Registrarse.jsx
@@ -1,81 +1,58 @@
-import React, {
-	TouchableOpacity,
-	View,
-	Image,
-	StyleSheet,
-	Text,
-} from "react-native"
+import * as React from 'react';
+import { TextInput, Text } from 'react-native-paper';
+import { Picker } from '@react-native-picker/picker';
+import Button from 'react-native';
+
 
 const Registrarse = ({ navigation }) => {
-	function redirect() {
-		navigation.navigate("LogIn")
+	const [text, setText] = React.useState("");
+	const [value, setValue] = React.useState("")
+
+	function onClick() {
+		navigation.navigate("Login")
 	}
 
 	return (
 		<>
-			<View style={styles.container}>
-				<TouchableOpacity
-					onPress={redirect}
-					style={{
-						width: 200,
-						height: 200,
-						flex: 2,
-						marginLeft: 20,
-						marginTop: 20,
-					}}
-				>
-					<View style={styles.Person}>
-						<Text style={styles.Texto}>Registrarse mediante persona</Text>
-					</View>
-					<Image
-						style={styles.img}
-						source={require("../assets/registerIcon.png")}
-					></Image>
-				</TouchableOpacity>
-				<TouchableOpacity
-					onPress={redirect}
-					style={{ width: 200, height: 200, flex: 2, marginLeft: 20 }}
-				>
-					<View style={styles.Person}>
-						<Text style={styles.Texto}> Registrarse mediante refugio </Text>
-					</View>
-					<Image
-						style={styles.img}
-						source={require("../assets/houseIcon.png")}
-					></Image>
-				</TouchableOpacity>
-			</View>
+			<TextInput
+				label="Nombre"
+				onChangeText={text => setText(text)}
+			/>
+			<TextInput
+				label="Apellido"
+				onChangeText={text => setText(text)}
+			/>
+			<TextInput
+				label="Email"
+				onChangeText={text => setText(text)}
+			/>
+			<TextInput
+				label="Password"
+				onChangeText={text => setText(text)}
+				secureTextEntry={true}
+			/>
+			<Text>
+				Tipo Usuario
+			</Text>
+			<Picker
+				selectedValue={value}
+				onValueChange={(itemValue) => setValue(itemValue)}
+			>
+				<Picker.Item label="Refugio" value="" />
+				<Picker.Item label="Persona" value="" />
+			</Picker>
+			
+			{/* <Button
+				onPress={onClick}
+				title="Enviar"
+				color="#841584"
+				accessibilityLabel="hola"
+			/> */}
+
 		</>
-	)
-}
+		
 
-const styles = StyleSheet.create({
-	img: {
-		width: 60,
-		height: 70,
-		aspectRatio: 0.9,
-		flex: 1,
-	},
-	Texto: {
-		// marginTop: "100px",
-		color: "#000",
-		fontSize: 15,
-		fontWeight: "bold",
-		textAlign: "center",
-		// fontSize: "72px",
-	},
-	container: {
-		backgroundColor: "#cdfdfd",
-		flexDirection: "column",
-		textAlign: "center",
-		padding: 20,
-		flex: 1,
-	},
-	Person: {
-		fontSize: 25,
-		fontfamily: "Monospace",
-		textAlign: "center",
-	},
-})
+	);
+};
 
-export default Registrarse
+export default Registrarse;
