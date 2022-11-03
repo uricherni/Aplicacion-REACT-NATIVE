6ddb76bb18aab6adfec3e77e2ae594232f980133
pbottino <pbottino95@gmail.com>
2022-11-03 19:14:41 -0300

    registrar ok

--- screens/Registrarse.jsx
+++ screens/Registrarse.jsx
@@ -1,58 +1,43 @@
-import * as React from 'react';
-import { TextInput, Text } from 'react-native-paper';
-import { Picker } from '@react-native-picker/picker';
-import Button from 'react-native';
-
+import React, { useState } from 'react'
+import { TextInput, Text } from 'react-native-paper'
+import { Picker } from '@react-native-picker/picker'
+import { Button } from 'react-native'
 
 const Registrarse = ({ navigation }) => {
-	const [text, setText] = React.useState("");
-	const [value, setValue] = React.useState("")
+	const [text, setText] = useState('')
+	const [value, setValue] = useState('')
 
 	function onClick() {
-		navigation.navigate("Login")
+		navigation.navigate('Login')
 	}
 
 	return (
 		<>
-			<TextInput
-				label="Nombre"
-				onChangeText={text => setText(text)}
-			/>
-			<TextInput
-				label="Apellido"
-				onChangeText={text => setText(text)}
-			/>
-			<TextInput
-				label="Email"
-				onChangeText={text => setText(text)}
-			/>
+			<TextInput label="Nombre" onChangeText={(text) => setText(text)} />
+			<TextInput label="Apellido" onChangeText={(text) => setText(text)} />
+			<TextInput label="Email" onChangeText={(text) => setText(text)} />
 			<TextInput
 				label="Password"
-				onChangeText={text => setText(text)}
+				onChangeText={(text) => setText(text)}
 				secureTextEntry={true}
 			/>
-			<Text>
-				Tipo Usuario
-			</Text>
+			<Text>Tipo Usuario</Text>
 			<Picker
 				selectedValue={value}
 				onValueChange={(itemValue) => setValue(itemValue)}
 			>
-				<Picker.Item label="Refugio" value="" />
-				<Picker.Item label="Persona" value="" />
+				<Picker.Item label="Refugio" value="refugio" />
+				<Picker.Item label="Persona" value="persona" />
 			</Picker>
-			
-			{/* <Button
+
+			<Button
 				onPress={onClick}
 				title="Enviar"
 				color="#841584"
 				accessibilityLabel="hola"
-			/> */}
-
+			/>
 		</>
-		
+	)
+}
 
-	);
-};
-
-export default Registrarse;
+export default Registrarse
