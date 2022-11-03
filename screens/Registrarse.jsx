2ad34bfc9ec1b339cc610c5d302517e77a074656
pbottino <pbottino95@gmail.com>
2022-11-03 20:03:32 -0300

    registro ok, falta el navigate

--- screens/Registrarse.jsx
+++ screens/Registrarse.jsx
@@ -2,32 +2,54 @@ import React, { useState } from 'react'
 import { TextInput, Text } from 'react-native-paper'
 import { Picker } from '@react-native-picker/picker'
 import { Button } from 'react-native'
+import axios from 'axios'
 
 const Registrarse = ({ navigation }) => {
-	const [text, setText] = useState('')
-	const [value, setValue] = useState('')
+	const [nombre, setNombre] = useState('')
+	const [apellido, setApellido] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [tipoUsuario, setTipoUsuario] = useState('')
 
-	function onClick() {
-		navigation.navigate('Login')
+	async function onClick() {
+		const data = {
+			Nombre: nombre,
+			Apellido: apellido,
+			Email: email,
+			Password: password,
+			TipoUsuario: tipoUsuario,
+			IdRefugio: 1,
+		}
+
+		const { status } = await axios.post(
+			'http://192.168.0.115:5000/Usuario',
+			data
+		)
+		if (status === 201) {
+			console.log('creo el usuario')
+			// navigation.navigate('/')
+		} else {
+			console.log('hubo un error')
+		}
 	}
 
 	return (
 		<>
-			<TextInput label="Nombre" onChangeText={(text) => setText(text)} />
-			<TextInput label="Apellido" onChangeText={(text) => setText(text)} />
-			<TextInput label="Email" onChangeText={(text) => setText(text)} />
+			<TextInput label="Nombre" onChangeText={(text) => setNombre(text)} />
+			<TextInput label="Apellido" onChangeText={(text) => setApellido(text)} />
+			<TextInput label="Email" onChangeText={(text) => setEmail(text)} />
 			<TextInput
 				label="Password"
-				onChangeText={(text) => setText(text)}
+				onChangeText={(text) => setPassword(text)}
 				secureTextEntry={true}
 			/>
 			<Text>Tipo Usuario</Text>
 			<Picker
-				selectedValue={value}
-				onValueChange={(itemValue) => setValue(itemValue)}
+				selectedValue={tipoUsuario}
+				onValueChange={(itemValue) => setTipoUsuario(itemValue)}
 			>
-				<Picker.Item label="Refugio" value="refugio" />
-				<Picker.Item label="Persona" value="persona" />
+				<Picker.Item label="Refugio" value={true} />
+				<Picker.Item label="Persona" value={false} />
 			</Picker>
 
 			<Button
